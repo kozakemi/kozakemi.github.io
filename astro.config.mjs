e5572eec5a09ebfe9517b6c17e1f77c28dbe171a
Kozakemi <kemikoza@gmail.com>
2024-11-11 01:43:31 +0800

    [fate] 更新域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kozakemi.gitlab.io',
+  site: 'https://kozakemi.top',
   base: '/',
   trailingSlash: 'always',
   integrations: [
